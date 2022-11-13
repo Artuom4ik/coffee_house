f7415ac2009316d15b46ba9fc02e62d53801fd30
Artuom4ik <masterskayaigr123@gmail.com>
2022-11-13 12:21:25 +0300

    Изменил отступы по PEP8.

--- main.py
+++ main.py
@@ -8,74 +8,75 @@ import requests
 import folium
 
 
-load_dotenv()
-
 def fetch_coordinates(apikey, address):
-  base_url = "https://geocode-maps.yandex.ru/1.x"
-  response = requests.get(base_url,
-                          params={
-                            "geocode": address,
-                            "apikey": apikey,
-                            "format": "json",
-                          })
-  response.raise_for_status()
-  found_places = response.json(
-  )['response']['GeoObjectCollection']['featureMember']
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url,
+                            params={
+                              "geocode": address,
+                              "apikey": apikey,
+                              "format": "json",
+                            })
+    response.raise_for_status()
+    found_places = response.json(
+    )['response']['GeoObjectCollection']['featureMember']
 
-  if not found_places:
-    return None
+    if not found_places:
+      return None
 
-  most_relevant = found_places[0]
-  lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-  return lat, lon
-
-
-with open("coffee.json", "r", encoding="CP1251") as my_file:
-  file_contents = my_file.read()
-  coffee_shops = json.loads(file_contents)
-
-user_address = input('Где вы находитесь? ')
-apikey = os.environ['APIkey']
-user_coords = fetch_coordinates(apikey, user_address)
-
-coffee_houses = []
-
-for coffee_shop in coffee_shops:
-  coffee_house = dict()
-  coffee_house['title'] = coffee_shop['Name']
-  coffee_house['distance'] = distance.distance(
-    user_coords,
-    (coffee_shop['Latitude_WGS84'], coffee_shop['Longitude_WGS84'])).km
-  coffee_house['latitude'] = coffee_shop['Latitude_WGS84']
-  coffee_house['longitude'] = coffee_shop['Longitude_WGS84']
-  coffee_houses.append(coffee_house)
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lat, lon
 
 
 def min_distance(coffee):
-  return coffee['distance']
+    return coffee['distance']
 
-sorted_coffee_shops = sorted(coffee_houses, key=min_distance)
-
-m = folium.Map(location=list(user_coords), zoom_start=600, tiles="Stamen Terrain")
-
-for cafe in sorted_coffee_shops[:5]:
-    coords_cafe = []
-    coords_cafe.append(cafe['latitude'])
-    coords_cafe.append(cafe['longitude'])
-    tooltip = cafe['title']
-    folium.Marker(
-        coords_cafe, 
-        popup="<i>Mt. Hood Meadows</i>",   
-        tooltip=tooltip,
-    ).add_to(m)
-  
-
-m.save("кофейни.html")
 
 def rendor_html():
-   with open('кофейни.html') as file:
-      return file.read()
+    with open('кофейни.html') as file:
+        return file.read()
 
-app = Flask(__name__)
-app.add_url_rule('/', 'Кофейни', rendor_html)
-app.run('0.0.0.0')
+
+def main():
+    load_dotenv()
+    with open("coffee.json", "r", encoding="CP1251") as my_file:
+        file_contents = my_file.read()
+        coffee_shops = json.loads(file_contents)
+
+    user_address = input('Где вы находитесь? ')
+    apikey = os.environ['APIkey']
+    user_coords = fetch_coordinates(apikey, user_address)
+
+    coffee_houses = []
+
+    for coffee_shop in coffee_shops:
+        coffee_house = dict()
+        coffee_house['title'] = coffee_shop['Name']
+        coffee_house['distance'] = distance.distance(
+          user_coords,
+          (coffee_shop['Latitude_WGS84'], coffee_shop['Longitude_WGS84'])).km
+        coffee_house['latitude'] = coffee_shop['Latitude_WGS84']
+        coffee_house['longitude'] = coffee_shop['Longitude_WGS84']
+        coffee_houses.append(coffee_house)
+    sorted_coffee_shops = sorted(coffee_houses, key=min_distance)
+
+    m = folium.Map(location=list(user_coords), zoom_start=600, tiles="Stamen Terrain")
+
+    for cafe in sorted_coffee_shops[:5]:
+        coords_cafe = []
+        coords_cafe.append(cafe['latitude'])
+        coords_cafe.append(cafe['longitude'])
+        tooltip = cafe['title']
+        folium.Marker(
+            coords_cafe, 
+            popup="<i>Mt. Hood Meadows</i>",   
+            tooltip=tooltip,
+        ).add_to(m)
+    m.save("кофейни.html") 
+    app = Flask(__name__)
+    app.add_url_rule('/', 'Кофейни', rendor_html)
+    app.run('0.0.0.0')
+
+
+if __name__ == "__main__":
+    main()
